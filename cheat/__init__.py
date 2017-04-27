927a384badb4d9cb3a7ec1a7c4b3da444b0332d6
Fredrik Larsen <fredrik.h.larsen@gmail.com>
2017-04-27 14:44:15 +0200

    Fixes version output from bin/cheat

--- cheat/__init__.py
+++ cheat/__init__.py
@@ -1,5 +1,9 @@
 # encoding: utf-8
 """ The cheatsheet package. """
-import pkg_resources as _pkg
 
-version = _pkg.get_distribution("cheat").parsed_version._version
+def get_version():
+    import pkg_resources as _pkg
+    return _pkg.get_distribution("cheat").parsed_version._version
+
+
+version = get_version()
