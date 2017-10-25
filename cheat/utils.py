58132a8498fb12b6c6d2b8c33d8c99b4ceba7ef1
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2017-10-25 20:26:51 +0200

    Add filters setting
    Refactors cheat/utils colorize functions.
    
    CHEATSTYLE can now be an ordered list of style names to try
    CHEATFILTERS can now be a mapping of filters to apply, if available

--- cheat/utils.py
+++ cheat/utils.py
@@ -3,16 +3,68 @@
 from __future__ import unicode_literals, print_function
 
 import os
+from collections import OrderedDict
+
+try:
+    from pygments import highlight
+    from pygments.filters import get_filter_by_name
+    from pygments.formatters import (TerminalFormatter,
+                                     Terminal256Formatter)
+    # from pygments.formatters.other import TerminalTrueColorFormatter
+    # from pygments.formatters.other import RawTokenFormatter
+    from pygments.lexers import get_lexer_for_filename, get_lexer_by_name
+    from pygments.styles import get_style_by_name
+
+    COLORS = True
+except ImportError:
+    COLORS = False
+
 
 default_lexer_name = 'markdown'
 default_style_name = 'default'
 
-FILTERS = {
-    'markdown': ['md_heading', ],
-}
+
+def no_highlight(text):
+    """ Return text unmodified. """
+    return text
 
 
-def _get_highlighter(filename, lexer_name, style=None):
+def parse_filters_setting(filters_setting):
+    """ Parse a filter map string.
+
+    :param str filters_setting:
+        Serialized map of filters to use for given lexers. Format:
+        "<lexer_name>=<filter_name>:..."
+
+    :return OrderedDict:
+        Returns an unserialized mapping of the filters_setting.
+    """
+    filters = OrderedDict()
+    for filter_setting in (filters_setting or '').split(os.pathsep):
+        lexer_name, _, filter_name = filter_setting.partition('=')
+        filter_list = filters.setdefault(lexer_name, list())
+        if filter_name not in filter_list:
+            filter_list.append(filter_name)
+    return filters
+
+
+def parse_styles_setting(styles_setting):
+    """ Parse a styles list string.
+
+    :param str styles_setting:
+        Serialized list of styles to use, ordered by preference.
+        Format: "<style_name>:..."
+
+    :return list:
+        Returns a list of style names.
+    """
+    seen = set()
+    seen_add = seen.add
+    return [style_name for style_name in styles_setting.split(os.pathsep)
+            if not (style_name in seen or seen_add(style_name))]
+
+
+def _get_highlighter(filename, lexer_name=None, styles=None, filters=None):
     """ Get highlighter.
 
     Creates a highlighter for, if available. The highlighter will use the first
@@ -23,27 +75,15 @@ def _get_highlighter(filename, lexer_name, style=None):
 
     :param string filename: A filename to fetch highlighter for.
     :param string name: A fallback lexer name to use for highlighting.
+    :param list styles: A list of preferred styles, in order
+    :param dict filters: A mapping from lexer names to a list of filters
 
     :return callable:
         A function that takes one parameter, the string to highlight.
 
     """
-    # A function that just returns the text unmodified, if we're unable to do
-    # highlighting:
-    def no_highlight(text):
-        return text
-
     # Import the highlight tools
-    try:
-        from pygments import highlight
-        from pygments.formatters import (TerminalFormatter,
-                                         Terminal256Formatter,
-                                         TerminalTrueColorFormatter)
-        from pygments.formatters.other import RawTokenFormatter
-        from pygments.lexers import get_lexer_for_filename, get_lexer_by_name
-        from pygments.styles import get_style_by_name
-        from pygments.filters import get_filter_by_name
-    except ImportError:
+    if not COLORS:
         return no_highlight
 
     # Find available lexer object
@@ -62,37 +102,35 @@ def _get_highlighter(filename, lexer_name, style=None):
 
     formatter = TerminalFormatter()
 
-    if style:
+    for style in (styles or []):
         try:
-            style = get_style_by_name(style)
+            formatter = Terminal256Formatter(style=get_style_by_name(style))
+            break
         except:
-            # Probably no such style
-            style = get_style_by_name(default_style_name)
-        # formatter = TerminalTrueColorFormatter(style=style)
-        formatter = Terminal256Formatter(style=style)
+            continue
 
-    filters = FILTERS.get(lexer.name, list())
-    for fn in filters:
+    for filter_name in filters.get(lexer.name, list()):
         try:
-            lexer.add_filter(get_filter_by_name(fn))
+            lexer.add_filter(get_filter_by_name(filter_name))
         except:
             pass
 
-#   return lambda text: (
-#       highlight(text, lexer, RawTokenFormatter()) +
-#       highlight(text, lexer, formatter))
     return lambda text: highlight(text, lexer, formatter)
 
 
 def colorize(sheet_content, filename=''):
     """ Colorizes cheatsheet content if so configured """
     do_colorize = os.environ.get('CHEATCOLORS', False)
-    style = os.environ.get('CHEATSTYLE', None)
+    styles = os.environ.get('CHEATSTYLE', None)
+    filters = os.environ.get('CHEATFILTERS', None)
 
     if not do_colorize:
         return sheet_content
 
-    highlight = _get_highlighter(filename, do_colorize, style=style)
+    highlight = _get_highlighter(filename,
+                                 lexer_name=do_colorize,
+                                 styles=parse_styles_setting(styles),
+                                 filters=parse_filters_setting(filters))
     return highlight(sheet_content)
 
 
