77dde6fe21617773e96f3d608d1af0da48178c00
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2020-07-10 16:34:19 +0200

    Fix issue with distutils and setuptools_scm
    Changes setup.py to use setuptools over distutils, which should fix an
    issue where setuptools_scm doesn't work.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 """ install cheat. """
 
-from distutils.core import setup
+import setuptools
 import os
 
 
@@ -15,27 +15,34 @@ def get_requirements(filename):
                 yield req_line
 
 
-setup(
-    name='cheat',
-    # version=get_version_number(),
-    author='Chris Lane',
-    author_email='chris@chris-allen-lane.com',
-    license='GPL3',
-    description=('cheat allows you to create and view interactive cheatsheets '
-                 'on the command-line. It was designed to help remind *nix '
-                 'system administrators of options for commands that they use '
-                 'frequently, but not frequently enough to remember.'),
-    url='https://github.com/chrisallenlane/cheat',
-    use_scm_version=True,
-    packages=['cheat', ],
-    data_files=[
-        ('etc/cheat/autocomplete', ['autocomplete/cheat.bash',
-                                    'autocomplete/cheat.fish',
-                                    'autocomplete/cheat.zsh']),
-        ('share/cheatsheets', [os.path.join('cheatsheets', f) for f
-                               in os.listdir('cheatsheets')]),
-    ],
-    scripts=['bin/cheat'],
-    setup_requires=['setuptools_scm'],
-    install_requires=list(get_requirements('requirements.txt')),
-)
+def main():
+    setuptools.setup(
+        name='cheat',
+        # version=get_version_number(),
+        author='Chris Lane',
+        author_email='chris@chris-allen-lane.com',
+        license='GPL3',
+        description=(
+            'cheat allows you to create and view interactive cheatsheets '
+            'on the command-line. It was designed to help remind *nix '
+            'system administrators of options for commands that they use '
+            'frequently, but not frequently enough to remember.'
+        ),
+        url='https://github.com/chrisallenlane/cheat',
+        use_scm_version=True,
+        packages=['cheat', ],
+        data_files=[
+            ('etc/cheat/autocomplete', ['autocomplete/cheat.bash',
+                                        'autocomplete/cheat.fish',
+                                        'autocomplete/cheat.zsh']),
+            ('share/cheatsheets', [os.path.join('cheatsheets', f) for f
+                                   in os.listdir('cheatsheets')]),
+        ],
+        scripts=['bin/cheat'],
+        setup_requires=['setuptools_scm'],
+        install_requires=list(get_requirements('requirements.txt')),
+    )
+
+
+if __name__ == '__main__':
+    main()
