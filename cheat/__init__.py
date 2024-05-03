bd6157c22e2674c4419e4d1e3e6302e38672cad3
Fredrik Larsen <fredrik.h.larsen@gmail.com>
2024-05-03 11:44:30 +0200

    Fix setup and versioning for Python 3.12

--- cheat/__init__.py
+++ cheat/__init__.py
@@ -1,8 +1,4 @@
 # encoding: utf-8
 """ The cheatsheet package. """
-from pkg_resources import get_distribution, DistributionNotFound
 
-try:
-    __version__ = get_distribution('cheat').version
-except DistributionNotFound:
-    __version__ = None
+__version__ = '2.4.0'
--- setup.py
+++ setup.py
@@ -1,36 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
-""" install cheat. """
-
-import setuptools
 import os
+import setuptools
 
-
-def get_requirements(filename):
-    """ Read requirements from file. """
-    with open(filename, 'r') as reqfile:
-        for req_line in reqfile.readlines():
-            req_line = req_line.strip()
-            if req_line:
-                yield req_line
-
-
-def main():
+if __name__ == '__main__':
     setuptools.setup(
-        name='cheat',
-        # version=get_version_number(),
-        author='Chris Lane',
-        author_email='chris@chris-allen-lane.com',
-        license='GPL3',
-        description=(
-            'cheat allows you to create and view interactive cheatsheets '
-            'on the command-line. It was designed to help remind *nix '
-            'system administrators of options for commands that they use '
-            'frequently, but not frequently enough to remember.'
-        ),
-        url='https://github.com/chrisallenlane/cheat',
-        use_scm_version=True,
-        packages=['cheat', ],
         data_files=[
             ('etc/cheat/autocomplete', ['autocomplete/cheat.bash',
                                         'autocomplete/cheat.fish',
@@ -39,10 +13,4 @@ def main():
                                    in os.listdir('cheatsheets')]),
         ],
         scripts=['bin/cheat'],
-        setup_requires=['setuptools_scm'],
-        install_requires=list(get_requirements('requirements.txt')),
     )
-
-
-if __name__ == '__main__':
-    main()
